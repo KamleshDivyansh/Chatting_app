7bc90595572c4d00d5380e3c9006b34cba077269
KamleshDivyansh <divyanshkamlesh70@gmail.com>
2025-06-19 12:32:03 +0530

    Chating app

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || '082022@Dk',
   database: process.env.DB_NAME || 'chat_app',
   waitForConnections: true,
   connectionLimit: 10,
